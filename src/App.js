68b8522daa34e986c914426c1c542aee86b91afa
Dhanush MC <dhanushmc93@gmail.com>
2023-10-13 16:24:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,74 @@
-import './App.css';
-import Button from '@mui/material/Button';
-import { Header } from './Header';
-import { Stickers } from './Stickers';
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import WebIcon from '@mui/icons-material/Web';
-import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
-import EmailIcon from '@mui/icons-material/Email';
-import { About } from './About';
-import { Projects } from './Projects';
-import { Footer } from './Footer';
-
-
-
+import "./App.css";
+// import Button from "@mui/material/Button";
+import { useState } from "react";
+import { Header } from "./Header";
+import { Stickers } from "./Stickers";
+import { Link, NavLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import WebIcon from "@mui/icons-material/Web";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import EmailIcon from "@mui/icons-material/Email";
+import { About } from "./About";
+import { Projects } from "./Projects";
+import { Footer } from "./Footer";
+// import ListIcon from "@mui/icons-material/List";
+// import CloseIcon from "@mui/icons-material/Close";
 
 function App() {
+  const [navopen, setNavopen] = useState("");
+  // const [navclose, setClose] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="App">
-      <div className='nav-bac'>
-    <div className='nav-bar'>
-    <div>
-      <img className='sign-img' src='https://see.fontimg.com/api/renderfont4/z8xjG/eyJyIjoiZnMiLCJoIjoxNzUsInciOjEyNTAsImZzIjoxNDAsImZnYyI6IiM4MTI4MjAiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/RGhhbnVzaA/darlington-demo.png' alt='skill-img'/>
+      <div className="nav-bac">
+        <nav>
+          <img
+            className="title sign-img"
+            src="https://see.fontimg.com/api/renderfont4/z8xjG/eyJyIjoiZnMiLCJoIjoxNzUsInciOjEyNTAsImZzIjoxNDAsImZnYyI6IiM4MTI4MjAiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/RGhhbnVzaA/darlington-demo.png"
+            alt="skill-img"
+          />
+          <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul className={menuOpen ? "open" : ""}>
+            <li>
+              <a className="nav-font" href="#h" to="/about">
+                Home
+              </a>
+            </li>
+            <li>
+              <a to="/services" href="#a">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#s" to="/contact">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#p" to="/contact">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#f" to="/contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <Header />
+        <About />
+        <Stickers />
+        <Projects />
+        <Footer />
+      </div>
     </div>
-    <div className='nav-but'>
-      <Button color="inherit"  className='nav-but-color' href='#h' size="large">< HomeIcon className="nav-ty-bu"/> &nbsp; Home</Button>
-      <Button  color="inherit"  className='nav-but-color' href='#a' size="large"><InfoIcon className="nav-ty-bu"/> &nbsp; About</Button>
-      <Button color="inherit" className='nav-but-color' href='#s' size="large" ><EmojiObjectsIcon className="nav-ty-bu"/>&nbsp; Skills</Button>
-      <Button color="inherit" className='nav-but-color' href='#p' size="large" ><WebIcon className="nav-ty-bu"/>&nbsp; Projects</Button>
-      <Button color="inherit" className='nav-but-color' href='#f' size="large" ><EmailIcon className="nav-ty-bu"/>&nbsp; Contact</Button>
-    </div>
-    </div>    
-    <Header/>
-    <About/>
-    <Stickers/>
-    <Projects/>
-    <Footer/>
-    </div></div>
-
-
-
   );
 }
-
 
 export default App;
---
+++ src/dummy.js
@@ -0,0 +1,79 @@
+{
+  /* <div className="nav-bac">
+  <div className="nav-bar">
+    <div>
+      <img
+        className="sign-img"
+        src="https://see.fontimg.com/api/renderfont4/z8xjG/eyJyIjoiZnMiLCJoIjoxNzUsInciOjEyNTAsImZzIjoxNDAsImZnYyI6IiM4MTI4MjAiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/RGhhbnVzaA/darlington-demo.png"
+        alt="skill-img"
+      />
+    </div>
+    <div className="nav-but" id="dd">
+      <span className="span-bu">
+        <Button onClick={() => setNavopen(true)}>
+          <ListIcon />
+        </Button>
+      </span>
+      {navopen ? (
+        <div className="nav-bb">
+          <span className="span-bu-cl">
+            <Button onClick={() => setNavopen(false)}>
+              <CloseIcon />
+            </Button>
+          </span>
+          <Button
+            color="inherit"
+            className="nav-but-color"
+            href="#h"
+            size="large"
+          >
+            <HomeIcon className="nav-ty-bu" /> &nbsp; Home
+          </Button>
+          <Button
+            color="inherit"
+            className="nav-but-color"
+            href="#a"
+            size="large"
+          >
+            <InfoIcon className="nav-ty-bu" /> &nbsp; About
+          </Button>
+          <Button
+            color="inherit"
+            className="nav-but-color"
+            href="#s"
+            size="large"
+          >
+            <EmojiObjectsIcon className="nav-ty-bu" />
+            &nbsp; Skills
+          </Button>
+          <Button
+            color="inherit"
+            className="nav-but-color"
+            href="#p"
+            size="large"
+          >
+            <WebIcon className="nav-ty-bu" />
+            &nbsp; Projects
+          </Button>
+          <Button
+            color="inherit"
+            className="nav-but-color"
+            href="#f"
+            size="large"
+          >
+            <EmailIcon className="nav-ty-bu" />
+            &nbsp; Contact
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  </div>
+  <Header />
+  <About />
+  <Stickers />
+  <Projects />
+  <Footer />
+</div>; */
+}
